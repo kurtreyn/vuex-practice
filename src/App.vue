78201587fa3ecf28fd65fb66c6f732807e25632e
kurtreyn <kurtreyn@yahoo.com>
2022-05-03 16:15:31 -0400

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProductListOne :products="products" />
+  <ProductListTwo :products="products" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ProductListOne,
+    ProductListTwo,
+  },
+  name: 'App',
+  data() {
+    return {
+      products: [
+        { name: 'Banana Skin', price: 20 },
+        { name: 'Shiny Star', price: 40 },
+        { name: 'Green Shells', price: 60 },
+        { name: 'Red Shells', price: 80 },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
